240c4d651ec7e9816ab09b448ca60c7bc312881a
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 06:33:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,18 @@ class Student
     new_student = self.new(row[1], row[2], row[0])
   end
 
+  def self.find_by_name(name)
+  # find the student in the database given a name
+  # return a new instance of the Student class
+  sql = <<-SQL
+  SELECT * FROM students WHERE name=? LIMIT 1
+  SQL
+  row = DB[:conn].execute(sql,name).flatten
+  #binding.pry
+  self.new_from_db(row)
+
+end
+
   def initialize(name, grade, id=nil)
     @name=name
     @grade=grade
